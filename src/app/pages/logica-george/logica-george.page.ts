5400990e988902bd426a0b369eab1fe693ff74a2
nerPatricia <pcostak@gmail.com>
2020-09-21 00:03:59 -0300

    add drag and drop e logica george

--- src/app/pages/logica-george/logica-george.page.ts
+++ src/app/pages/logica-george/logica-george.page.ts
@@ -1,20 +1,27 @@
-import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-logica-george',
   templateUrl: 'logica-george.page.html',
   styleUrls: ['logica-george.page.scss'],
 })
-export class LogicaGeorgePage {
+export class LogicaGeorgePage implements OnInit {
   frases = [
-    'O patrão não conferiu o depósito ou ele não tinha ligado para a polícia ainda',
-    'Ele já tinha ligado pra polícia'
+    'Se a jóia é roubada, ela não tem selo de autencidade',
+    'Se ele busca algo para a coleção, então ele é um colecionador',
+    'Se ele é um colecionador, então ele precisa do selo de autenticidade',
+    'Ele veio buscar algo pra coleção'
   ];
-  resultado;
+  resultados = [];
+  subsResultados: Observable<any[]>;
 
   constructor(
+    private navCtrl: NavController,
     private route: ActivatedRoute,
     private router: Router,
   ) {
@@ -25,17 +32,23 @@ export class LogicaGeorgePage {
     // });
   }
 
-  drop(event: CdkDragDrop<string[]>) {
+  ngOnInit() {
+    swal.fire('Resolva o caso', 'Arraste as frases na ordem correta de acordo com a formula lógica e chegue a solução do caso!');
+  }
+
+  drop(event: CdkDragDrop<string[]>, index?) {
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
     } else {
       if (event.previousIndex === 0 && event.currentIndex === 0) {
-        this.resultado = '';
+        this.resultados[index] = '';
       } else {
-        this.resultado = event.previousContainer.data[event.previousIndex];
+        this.resultados[index] = event.previousContainer.data[event.previousIndex];
       }
     }
   }
 
-  ionViewDidEnter() {}
+  avancar() {
+    this.navCtrl.navigateRoot(['logica-joanne']);
+  }
 }
